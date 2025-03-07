b5d87561d0c4b1aaca750f9477805481bd31d670
Alejandro_Wong <121511772+Alejandro-Wong@users.noreply.github.com>
2025-03-06 21:27:23 -0500

    added missing backtesting.py Backtest parameters

--- stratdev/backtest/run_backtest.py
+++ stratdev/backtest/run_backtest.py
@@ -9,7 +9,16 @@ trades = './trades/'
 stats = './stats/'
 pdfs = './pdfs/'
 
-def run_backtest(strategy: object, symbols: dict) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def run_backtest(strategy: object,
+                symbols: dict,
+                msc:str,
+                cash:int=10000,
+                comission:float=0.0,
+                margin:float=1.0,
+                trade_on_close:bool=False,
+                hedging:bool=False,
+                exclusive_orders:bool=False,
+                ) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Runs multi-ticker backtest of chosen strategy on chosen symbols. Results of backtest are
     saved to appropriate folders.
@@ -20,7 +29,12 @@ def run_backtest(strategy: object, symbols: dict) -> tuple[pd.DataFrame, pd.Data
     btpy = BacktestingPy(strategy, symbols, html_path=htmls, stats_path=stats, trades_path=trades)
 
     # Multi-ticker backtest 
-    bt_stats, bt_trades, bt_equity_curves = btpy.multi_ticker_backtest()
+    bt_stats, bt_trades, bt_equity_curves = btpy.multi_ticker_backtest(cash=cash,
+                                                                       commission=comission,
+                                                                       margin=margin,
+                                                                       trade_on_close=trade_on_close,
+                                                                       hedging=hedging,
+                                                                       exclusive_orders=exclusive_orders)
 
     # Analysis
     bt_return_v_dd = analysis.returns_vs_drawdowns(btpy, bt_stats)
@@ -28,13 +42,13 @@ def run_backtest(strategy: object, symbols: dict) -> tuple[pd.DataFrame, pd.Data
     bt_ratios = analysis.ratios(btpy, bt_stats)
 
     # Export to pdf
-    with PdfPages(f'{pdfs}returns_vs_drawdowns.pdf') as pdf:
+    with PdfPages(f'{pdfs}returns_vs_drawdowns_{msc}.pdf') as pdf:
         pdf.savefig(bt_return_v_dd)
 
-    with PdfPages(f'{pdfs}equity_curves.pdf') as pdf:
+    with PdfPages(f'{pdfs}equity_curves_{msc}.pdf') as pdf:
         pdf.savefig(bt_ec_plot)
 
-    with PdfPages(f'{pdfs}ratios.pdf') as pdf:
+    with PdfPages(f'{pdfs}ratios_{msc}.pdf') as pdf:
         pdf.savefig(bt_ratios)
 
     return bt_stats, bt_trades, bt_equity_curves
